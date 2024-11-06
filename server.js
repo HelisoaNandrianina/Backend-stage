42408c36a87a1011901fa21055ff60c6805d2edf
NandrianinaRadimimanana <hnradimimanana@gmail.com>
2024-11-06 06:03:21 -0800

    Premier commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+const userRoutes = require('./app/routes/manager.routes');
+const authentificationRoutes = require('./app/routes/authentification.routes');
+
+// Middleware pour les routes
+const verifyToken = require('./app/controllers/authMiddleware');
+
+const port = process.env.PORT || 3002;
+
+
+const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+var corsOptions = {
+    origin: `http://localhost:${port}`
+};
+
+app.use(cors(corsOptions));
+
+// Middleware pour les routes protégées
+app.use((req, res, next) => {
+    // On ignore les routes /login et /register
+    if (req.path === '/auth/login' || req.path === '/auth/register') {
+        return next();
+    } else {
+        verifyToken(req, res, next);
+    }
+})
+
+userRoutes(app);
+authentificationRoutes(app);
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
